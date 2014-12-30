f26a4d746fba4cc8b28bf7fcace61e941120389f
Slava Shklyaev <shk.slava@gmail.com>
2014-12-30 14:15:22 +0200

    Fix redirection problems

--- class-comments.php
+++ class-comments.php
@@ -330,11 +330,7 @@ class YouTubeComments {
 	* @return string	
 	*/		
 	function get_redirect_url() {
-		$s = empty($_SERVER['HTTPS']) ? '' : ($_SERVER['HTTPS'] == 'on') ? 's' : '';
-		$protocol = substr(strtolower($_SERVER['SERVER_PROTOCOL']), 0, strpos(strtolower($_SERVER['SERVER_PROTOCOL']), '/')) . $s;
-		$port = ($_SERVER['SERVER_PORT'] == '80') ? '' : (':'.$_SERVER['SERVER_PORT']);
-		$url = $protocol . '://' . $_SERVER['SERVER_NAME'] . $port . $_SERVER['REQUEST_URI'];
-		return filter_var($url, FILTER_SANITIZE_URL);
+		return $_SERVER['REQUEST_URI'];
 	}	
 	
 	/**
